33c19549f4562645613312047cb1e347c9f27eed
Andrew Austin <austin@impetuo.us>
2025-02-27 05:13:49 -0500

    added Plutonium - barebones per Claude 3.7 suggestions

---
+++ machines/plutonium/configuration.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  # Define hostname
+  networking.hostName = "Plutonium";
+  
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Environment path
+  environment.systemPath = [
+    "/opt/homebrew/bin"
+    "/opt/homebrew/sbin"
+  ];
+  
+  # System packages
+  environment.systemPackages = with pkgs; [
+    coreutils
+    gnused
+  ];
+  
+  # Set nix-darwin state version
+  system.stateVersion = 4;
+}
