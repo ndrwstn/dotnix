5d2274e64ab297333679866f84becbb900f0b1d4
Andrew Austin <austin@impetuo.us>
2025-03-15 09:46:33 -0400

    simulate user config flakes for nix-darwin - nix-darwin doesn't do per-user config (currently) - want to allow individual users to have separate homebrew folders - use lib.mkMerge to add all the `homebrew.nix` (user and system) files together

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -4,11 +4,53 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  # Get list of active users
+  usersDir = ../../users;
+  usersList = builtins.attrNames (builtins.readDir usersDir);
+  
+  # Function to check if a user has a darwin/homebrew.nix file
+  hasUserHomebrew = user: 
+    builtins.pathExists (usersDir + "/${user}/darwin/homebrew.nix");
+  
+  # Filter to users that have homebrew configs
+  usersWithHomebrew = builtins.filter hasUserHomebrew usersList;
+  
+  # Import user homebrew configs if they exist and extract the homebrew attribute
+  userHomebrewConfigs = map (user: 
+    let 
+      imported = import (usersDir + "/${user}/darwin/homebrew.nix") {
+        inherit config pkgs lib;
+      };
+    in
+      if builtins.hasAttr "homebrew" imported
+      then imported.homebrew
+      else lib.warn "User ${user}'s homebrew.nix doesn't contain a homebrew attribute" {}
+  ) usersWithHomebrew;
+  
+  # System homebrew config - extract the homebrew attribute
+  systemHomebrew = 
+    if builtins.pathExists ./homebrew.nix
+    then let 
+      imported = import ./homebrew.nix { inherit config pkgs lib; };
+    in
+      if builtins.hasAttr "homebrew" imported
+      then [imported.homebrew]
+      else lib.warn "System homebrew.nix doesn't contain a homebrew attribute" []
+    else [];
+  
+  # Combine all homebrew configurations
+  allHomebrewConfigs = systemHomebrew ++ userHomebrewConfigs;
+  
+  # Merge all homebrew configurations into a single config
+  mergedHomebrewConfig = lib.mkMerge allHomebrewConfigs;
+in {
   imports = [
-    ./homebrew.nix
     ./system.nix
   ];
+
+  # Apply the merged Homebrew config directly
+  homebrew = mergedHomebrewConfig;
 
   # System defaults
   system.defaults = {
---
+++ users/austin/darwin/homebrew.nix
@@ -0,0 +1,79 @@
+# systems/darwin/homebrew.nix
+{
+  config,
+  pkgs,
+  ...
+}: {
+  homebrew = {
+    enable = true;
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+    };
+    global = {
+      brewfile = true;
+    };
+    casks = [
+      "1password"
+      "appcleaner"
+      # "balenaetcher"
+      # "blender"
+      # "brave-browser"
+      # "chromium" # NOTE -- not installed in favor of 'elonston-chromium' (ungoogled version)
+      # "dbeaver-community"
+      # "discord"
+      # "eloston-chromium" # NOTE -- ungoogled version?
+      # "freecad"
+      # "gimp"
+      # "gqrx"
+      "ghostty"
+      # "inkscape"
+      # "jdownloader"
+      # "jordanbaird-ice"
+      "keka"
+      # "kicad"
+      # "krita"
+      "logi-options+"
+      "monitorcontrol"
+      # "mqttx"
+      "neovide"
+      # "numi"
+      # "obs"
+      # "obsidian"
+      # "orcaslicer"
+      # "plex"
+      # "pycharm-ce"
+      # "raycast"
+      "sublime-merge"
+      "sublime-text"
+      # "transmission"
+      # "transmission-remote-gui"
+      # "vlc"
+    ];
+    brews = [
+      "displayplacer"
+      "ffmpeg"
+      "mas"
+    ];
+    masApps = {
+      # NOTE - homebrew.masApps does not remove apps
+      "1Password for Safari" = 1569813296;
+      "Across" = 6444851827;
+      "AdBlock Plus" = 1432731683;
+      "Amphetamine" = 937984704;
+      # "GarageBand" = 682658836;
+      "Goodnotes" = 1444383602;
+      "Grab2Text" = 6475956137;
+      "Home Assistant" = 1099568401;
+      # "iMovie" = 408981434;
+      # "Keynote" = 409183694;
+      # "LiquidText" = 922765270;
+      # "Numbers" = 409203825;
+      # "Pages" = 409201541;
+      "RECAP Uploader" = 1600281788;
+      "The Camelizer" = 1532579087;
+      "Things" = 904280696;
+      # "Xcode" = 497799835;
+    };
+  };
+}
