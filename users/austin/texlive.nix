6a40010c93aacf1ff6d211175fbbca5c2ae91a9f
Andrew Austin <austin@impetuo.us>
2025-03-20 12:13:58 -0400

    Add LaTeX packages so custom `uslegal` works.

--- users/austin/texlive.nix
+++ users/austin/texlive.nix
@@ -10,13 +10,31 @@ pkgs.texlive.combine {
     latex-bin
     # Basic packages
     babel-english
-    extsizes
     lualatex-math
     unicode-math
-    # Document elements
-    hyperref
     graphics
+    # Document elements
+    enumitem
+    nth
+    footmisc
+    hyperref
+    extsizes
     geometry
+    titlesec
     xcolor
+    setspace
+    fancyhdr
+    xkeyval
+    pdfpages
+    datetime2
+    pgfopts
+    multirow
+    makecell
+    nowidow
+    xpeek
+    xurl
+    pdflscape
+    epstopdf
+    epstopdf-pkg
     ;
 }
