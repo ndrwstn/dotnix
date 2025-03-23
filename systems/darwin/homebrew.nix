dd06044eae1b0d8f88c7b97e17227425f2ed8cb7
Andrew Austin <austin@impetuo.us>
2025-03-23 18:07:42 -0400

    fix path for homebrew

--- systems/darwin/homebrew.nix
+++ systems/darwin/homebrew.nix
@@ -4,5 +4,16 @@
   pkgs,
   ...
 }: {
-  # null file
+  # Add homebrew to system PATH
+  environment.systemPath =
+    if pkgs.stdenv.isAarch64
+    then [
+      "/opt/homebrew/bin"
+      "/opt/homebrew/sbin"
+    ]
+    else [
+      "/usr/local/bin"
+      "/usr/local/sbin"
+    ];
 }
+
