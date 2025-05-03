44b353a09ec87adfd4e8eee8c99e51ebf3d03fb7
Andrew Austin <austin@impetuo.us>
2025-05-03 18:36:25 -0400

    fix duplicate keymap + sort

--- users/austin/nixvim/keymaps.nix
+++ users/austin/nixvim/keymaps.nix
@@ -2,37 +2,37 @@
 {
   keymaps = [
     # Buffer Navigation
+    { key = "<leader>bd"; mode = "n"; action = "<cmd>bdelete<CR>"; options.desc = "Delete Buffer"; }
     { key = "<leader>bn"; mode = "n"; action = "<cmd>bnext<CR>"; options.desc = "Next Buffer"; }
     { key = "<leader>bp"; mode = "n"; action = "<cmd>bprevious<CR>"; options.desc = "Previous Buffer"; }
-    { key = "<leader>bd"; mode = "n"; action = "<cmd>bdelete<CR>"; options.desc = "Delete Buffer"; }
 
     # Harpoon
-    { key = "<leader>ha"; mode = "n"; action = "<cmd>lua require('harpoon'):list():add()<CR>"; options.desc = "Add File (Harpoon)"; }
-    { key = "<leader>hm"; mode = "n"; action = "<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>"; options.desc = "Toggle Quick Menu (Harpoon)"; }
     # { key = "<leader>hh"; mode = "n"; action = "<cmd>Telescope harpoon marks<CR>"; options.desc = "Show Marks (Harpoon Telescope)"; }
     { key = "<leader>h1"; mode = "n"; action = "<cmd>lua require('harpoon'):list():select(1)<CR>"; options.desc = "Go to mark 1 (Harpoon)"; }
     { key = "<leader>h2"; mode = "n"; action = "<cmd>lua require('harpoon'):list():select(2)<CR>"; options.desc = "Go to mark 2 (Harpoon)"; }
     { key = "<leader>h3"; mode = "n"; action = "<cmd>lua require('harpoon'):list():select(3)<CR>"; options.desc = "Go to mark 3 (Harpoon)"; }
     { key = "<leader>h4"; mode = "n"; action = "<cmd>lua require('harpoon'):list():select(4)<CR>"; options.desc = "Go to mark 4 (Harpoon)"; }
+    { key = "<leader>ha"; mode = "n"; action = "<cmd>lua require('harpoon'):list():add()<CR>"; options.desc = "Add File (Harpoon)"; }
+    { key = "<leader>hm"; mode = "n"; action = "<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<CR>"; options.desc = "Toggle Quick Menu (Harpoon)"; }
     { key = "<leader>hn"; mode = "n"; action = "<cmd>lua require('harpoon'):list():next()<CR>"; options.desc = "Next Mark (Harpoon)"; }
     { key = "<leader>hp"; mode = "n"; action = "<cmd>lua require('harpoon'):list():prev()<CR>"; options.desc = "Previous Mark (Harpoon)"; }
 
     # Neotree
     { key = "<leader>ef"; mode = "n"; action = "<cmd>Neotree action=focus source=filesystem position=left<CR>"; options.desc = "Show Files (Neotree)"; }
-    { key = "<leader>ex"; mode = "n"; action = "<cmd>Neotree action=close source=filesystem position=left<CR>"; options.desc = "Close Files (Neotree)"; }
     { key = "<leader>er"; mode = "n"; action = "<cmd>Neotree action=focus source=filesystem reveal=true position=left<CR>"; options.desc = "Reveal File (Neotree)"; }
+    { key = "<leader>ex"; mode = "n"; action = "<cmd>Neotree action=close source=filesystem position=left<CR>"; options.desc = "Close Files (Neotree)"; }
 
     # sops-nvim
     { key = "<leader>usd"; mode = "n"; action = "<cmd>SopsDecrypt<CR>"; options.desc = "Decrypt File (Sops)"; }
     { key = "<leader>use"; mode = "n"; action = "<cmd>SopsEncrypt<CR>"; options.desc = "Encrypt File (Sops)"; }
 
     # Telescope
+    { key = "<leader>f?"; mode = "n"; action = "<cmd>Telescope help_tags<CR>"; options.desc = "Find Help (Telescope)"; }
+    { key = "<leader>fb"; mode = "n"; action = "<cmd>Telescope buffers<CR>"; options.desc = "Find Buffers (Telescope)"; }
     { key = "<leader>ff"; mode = "n"; action = "<cmd>Telescope find_files<CR>"; options.desc = "Find Files (Telescope)"; }
     { key = "<leader>fg"; mode = "n"; action = "<cmd>Telescope live_grep<CR>"; options.desc = "Find Grep (Telescope)"; }
     { key = "<leader>fh"; mode = "n"; action = "<cmd>Telescope harpoon marks<CR>"; options.desc = "Find Marks (Telescope)"; }
-    { key = "<leader>fb"; mode = "n"; action = "<cmd>Telescope buffers<CR>"; options.desc = "Find Buffers (Telescope)"; }
-    { key = "<leader>fr"; mode = "n"; action = "<cmd>Telescope frecency<CR>"; options.desc = "Find Recent/Frequent (Telescope)"; }
-    { key = "<leader>fh"; mode = "n"; action = "<cmd>Telescope help_tags<CR>"; options.desc = "Find Help (Telescope)"; }
     { key = "<leader>fp"; mode = "n"; action = "<cmd>Telescope projects<CR>"; options.desc = "Find Projects (Telescope)"; }
+    { key = "<leader>fr"; mode = "n"; action = "<cmd>Telescope frecency<CR>"; options.desc = "Find Recent/Frequent (Telescope)"; }
   ];
 }
