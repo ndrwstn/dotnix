a125ea7cb824f1fade31ac4c2e4b4f268cdb6324
Andrew Austin <austin@impetuo.us>
2025-05-15 16:55:26 -0400

    cleanup
    - autoformat

--- users/austin/nixvim/options.nix
+++ users/austin/nixvim/options.nix
@@ -1,19 +1,19 @@
 # users/austin/nixvim/options.nix
 {
-	opts = {
-		tabstop = 2;
-		softtabstop = 2;
-		shiftwidth = 2;
-		expandtab = true;
-		number = true;
-		relativenumber = true;
-		termguicolors = true;
-		mouse = "a";	
-		linebreak = true;
-		breakindent = true;
-		showbreak = "↪ ";
-		signcolumn = "yes";
-		guifont = "Inconsolata Nerd Font:h17";
-	};
+  opts = {
+    tabstop = 2;
+    softtabstop = 2;
+    shiftwidth = 2;
+    expandtab = true;
+    number = true;
+    relativenumber = true;
     # wrap = true; # handled by wrapping-nvim in `./extra.nix`.
+    termguicolors = true;
+    mouse = "a";
+    linebreak = true;
+    breakindent = true;
+    showbreak = "↪ ";
+    signcolumn = "yes";
+    guifont = "Inconsolata Nerd Font:h17";
+  };
 }
