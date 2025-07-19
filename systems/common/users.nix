f7f39ed950ac5ca7f8478a0558ad421404e43203
Andrew Austin <austin@impetuo.us>
2025-07-19 15:25:01 -0400

    add passwords to users
    - should actually be using mutableUsers but
      leaving that for the future
    - passwords will be enforced on every nix-rebuild

--- systems/common/users.nix
+++ systems/common/users.nix
@@ -27,12 +27,14 @@
         # extraGroups = [ "networkmanager" "wheel" "disk" "plugdev" "dialout" ];
         extraGroups = [ "networkmanager" "wheel" "disk" "plugdev" ];
         home = "/home/austin";
+        hashedPassword = "$6$5PZLg16IXRSJaLiI$bU8OB6wng7ZvQcrkpX/x5pjX2GegtYN.YUIAibPvAtVq/nyGwfjLyGwV5GR2LCnEqytFzxxer6.fhAhO7G8lD1";
       };
 
       jessica = {
         isNormalUser = true;
         extraGroups = [ "networkmanager" ];
         home = "/home/jessica";
+        hashedPassword = "$6$CXDT2agbjBSyDEnL$VDtjFbPcD20UWSSOj/h2rrYq/SteAkYBZcQn2N7/8fQpgEJKkxR9MbGokLwUjWvslAcwRDIVUcyOg.0neMFMK1";
       };
     })
 
