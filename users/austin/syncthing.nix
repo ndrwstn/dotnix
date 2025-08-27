52a470869947d5e38489322ad72d88a78558d16d
Andrew Austin <austin@impetuo.us>
2025-08-27 19:07:29 -0400

    feat(syncthing): make all global options fully configurable through JSON secrets
    - Extract all ~40 global options from JSON secrets with inline defaults
    - Replace hardcoded XML values with configurable variables
    - Add special null-check handling for boolean options to handle jq falsy behavior
    - Implement special handling for minHomeDiskFree with separate value/unit extraction
    - Organize options into logical categories: discovery, bandwidth, relay/NAT,
      usage reporting, auto-update, performance, connection priorities, security, GUI
    - Enable full Syncthing customization through secrets/syncthing.json while
      maintaining sensible defaults for all options

--- users/austin/syncthing.nix
+++ users/austin/syncthing.nix
@@ -112,9 +112,86 @@ let
         # Read device IDs from secrets
         SHARED_CONFIG=$(cat "${sharedSecretPath}" 2>/dev/null || echo '{}')
         MACHINE_CONFIG=$(cat "${secretPath}" 2>/dev/null || echo '{}')
-    
+
         # Extract own device ID
         OWN_DEVICE_ID=$(echo "$MACHINE_CONFIG" | ${pkgs.jq}/bin/jq -r '.deviceId // empty' 2>/dev/null || echo "")
+
+        # Extract global options from SHARED_CONFIG with defaults
+        # Note: For booleans, we use explicit null checks because jq's // operator treats false as falsy
+        
+        # Discovery options
+        LISTEN_ADDRESS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.listenAddress // "default"')
+        GLOBAL_ANNOUNCE_SERVER=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.globalAnnounceServer // "default"')
+        GLOBAL_ANNOUNCE_ENABLED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.globalAnnounceEnabled as $val | if $val == null then true else $val end')
+        LOCAL_ANNOUNCE_ENABLED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.localAnnounceEnabled as $val | if $val == null then true else $val end')
+        LOCAL_ANNOUNCE_PORT=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.localAnnouncePort // 21027')
+        LOCAL_ANNOUNCE_MC_ADDR=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.localAnnounceMCAddr // "[ff12::8384]:21027"')
+        
+        # Bandwidth options
+        MAX_SEND_KBPS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.maxSendKbps // 0')
+        MAX_RECV_KBPS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.maxRecvKbps // 0')
+        LIMIT_BANDWIDTH_IN_LAN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.limitBandwidthInLan as $val | if $val == null then false else $val end')
+        
+        # Relays and NAT options
+        RELAYS_ENABLED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.relaysEnabled as $val | if $val == null then false else $val end')
+        NAT_ENABLED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.natEnabled as $val | if $val == null then false else $val end')
+        RELAY_RECONNECT_INTERVAL_M=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.relayReconnectIntervalM // 10')
+        NAT_LEASE_MINUTES=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.natLeaseMinutes // 60')
+        NAT_RENEWAL_MINUTES=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.natRenewalMinutes // 30')
+        NAT_TIMEOUT_SECONDS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.natTimeoutSeconds // 10')
+        
+        # Usage reporting options
+        UR_ACCEPTED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urAccepted // -1')
+        UR_SEEN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urSeen // 3')
+        UR_UNIQUE_ID=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urUniqueID // ""')
+        UR_URL=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urURL // "https://data.syncthing.net/newdata"')
+        UR_POST_INSECURELY=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urPostInsecurely as $val | if $val == null then false else $val end')
+        UR_INITIAL_DELAY_S=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.urInitialDelayS // 1800')
+        
+        # Update options
+        AUTO_UPGRADE_INTERVAL_H=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.autoUpgradeIntervalH // 12')
+        UPGRADE_TO_PRE_RELEASES=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.upgradeToPreReleases as $val | if $val == null then false else $val end')
+        RELEASES_URL=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.releasesURL // "https://upgrades.syncthing.net/meta.json"')
+        
+        # Performance options
+        RECONNECTION_INTERVAL_S=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.reconnectionIntervalS // 60')
+        KEEP_TEMPORARIES_H=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.keepTemporariesH // 24')
+        CACHE_IGNORED_FILES=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.cacheIgnoredFiles as $val | if $val == null then false else $val end')
+        PROGRESS_UPDATE_INTERVAL_S=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.progressUpdateIntervalS // 5')
+        TEMP_INDEX_MIN_BLOCKS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.tempIndexMinBlocks // 10')
+        TRAFFIC_CLASS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.trafficClass // 0')
+        SET_LOW_PRIORITY=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.setLowPriority as $val | if $val == null then true else $val end')
+        MAX_FOLDER_CONCURRENCY=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.maxFolderConcurrency // 0')
+        MAX_CONCURRENT_INCOMING_REQUEST_KIB=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.maxConcurrentIncomingRequestKiB // 0')
+        
+        # Connection priority options
+        CONNECTION_PRIORITY_TCP_LAN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityTcpLan // 10')
+        CONNECTION_PRIORITY_QUIC_LAN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityQuicLan // 20')
+        CONNECTION_PRIORITY_TCP_WAN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityTcpWan // 30')
+        CONNECTION_PRIORITY_QUIC_WAN=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityQuicWan // 40')
+        CONNECTION_PRIORITY_RELAY=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityRelay // 50')
+        CONNECTION_PRIORITY_UPGRADE_THRESHOLD=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionPriorityUpgradeThreshold // 0')
+        CONNECTION_LIMIT_ENOUGH=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionLimitEnough // 0')
+        CONNECTION_LIMIT_MAX=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.connectionLimitMax // 0')
+        
+        # Security options
+        INSECURE_ALLOW_OLD_TLS_VERSIONS=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.insecureAllowOldTLSVersions as $val | if $val == null then false else $val end')
+        CRASH_REPORTING_ENABLED=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.crashReportingEnabled as $val | if $val == null then false else $val end')
+        CRASH_REPORTING_URL=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.crashReportingURL // "https://crash.syncthing.net/newcrash"')
+        
+        # Other options
+        START_BROWSER=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.startBrowser as $val | if $val == null then false else $val end')
+        OVERWRITE_REMOTE_DEVICE_NAMES_ON_CONNECT=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.overwriteRemoteDeviceNamesOnConnect as $val | if $val == null then false else $val end')
+        ANNOUNCE_LAN_ADDRESSES=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.announceLANAddresses as $val | if $val == null then true else $val end')
+        SEND_FULL_INDEX_ON_UPGRADE=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.sendFullIndexOnUpgrade as $val | if $val == null then false else $val end')
+        STUN_KEEPALIVE_START_S=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.stunKeepaliveStartS // 180')
+        STUN_KEEPALIVE_MIN_S=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.stunKeepaliveMinS // 20')
+        STUN_SERVER=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.stunServer // "default"')
+        DATABASE_TUNING=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.databaseTuning // "auto"')
+        
+        # Special case: minHomeDiskFree needs separate value and unit extraction
+        MIN_HOME_DISK_FREE_VALUE=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.minHomeDiskFree.value // 1')
+        MIN_HOME_DISK_FREE_UNIT=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r '.options.minHomeDiskFree.unit // "%"')
     
         # Count available devices for logging
         DEVICE_COUNT=$(echo "$SHARED_CONFIG" | ${pkgs.jq}/bin/jq -r --arg self "${machineName}" '
@@ -254,59 +331,59 @@ let
         </gui>
         <ldap></ldap>
         <options>
-            <listenAddress>default</listenAddress>
-            <globalAnnounceServer>default</globalAnnounceServer>
-            <globalAnnounceEnabled>true</globalAnnounceEnabled>
-            <localAnnounceEnabled>true</localAnnounceEnabled>
-            <localAnnouncePort>21027</localAnnouncePort>
-            <localAnnounceMCAddr>[ff12::8384]:21027</localAnnounceMCAddr>
-            <maxSendKbps>0</maxSendKbps>
-            <maxRecvKbps>0</maxRecvKbps>
-            <reconnectionIntervalS>60</reconnectionIntervalS>
-            <relaysEnabled>false</relaysEnabled>
-            <relayReconnectIntervalM>10</relayReconnectIntervalM>
-            <startBrowser>false</startBrowser>
-            <natEnabled>false</natEnabled>
-            <natLeaseMinutes>60</natLeaseMinutes>
-            <natRenewalMinutes>30</natRenewalMinutes>
-            <natTimeoutSeconds>10</natTimeoutSeconds>
-            <urAccepted>-1</urAccepted>
-            <urSeen>3</urSeen>
-            <urUniqueID></urUniqueID>
-            <urURL>https://data.syncthing.net/newdata</urURL>
-            <urPostInsecurely>false</urPostInsecurely>
-            <urInitialDelayS>1800</urInitialDelayS>
-            <autoUpgradeIntervalH>12</autoUpgradeIntervalH>
-            <upgradeToPreReleases>false</upgradeToPreReleases>
-            <keepTemporariesH>24</keepTemporariesH>
-            <cacheIgnoredFiles>false</cacheIgnoredFiles>
-            <progressUpdateIntervalS>5</progressUpdateIntervalS>
-            <limitBandwidthInLan>false</limitBandwidthInLan>
-            <minHomeDiskFree unit="%">1</minHomeDiskFree>
-            <releasesURL>https://upgrades.syncthing.net/meta.json</releasesURL>
-            <overwriteRemoteDeviceNamesOnConnect>false</overwriteRemoteDeviceNamesOnConnect>
-            <tempIndexMinBlocks>10</tempIndexMinBlocks>
-            <trafficClass>0</trafficClass>
-            <setLowPriority>true</setLowPriority>
-            <maxFolderConcurrency>0</maxFolderConcurrency>
-            <crashReportingURL>https://crash.syncthing.net/newcrash</crashReportingURL>
-            <crashReportingEnabled>false</crashReportingEnabled>
-            <stunKeepaliveStartS>180</stunKeepaliveStartS>
-            <stunKeepaliveMinS>20</stunKeepaliveMinS>
-            <stunServer>default</stunServer>
-            <databaseTuning>auto</databaseTuning>
-            <maxConcurrentIncomingRequestKiB>0</maxConcurrentIncomingRequestKiB>
-            <announceLANAddresses>true</announceLANAddresses>
-            <sendFullIndexOnUpgrade>false</sendFullIndexOnUpgrade>
-            <connectionLimitEnough>0</connectionLimitEnough>
-            <connectionLimitMax>0</connectionLimitMax>
-            <insecureAllowOldTLSVersions>false</insecureAllowOldTLSVersions>
-            <connectionPriorityTcpLan>10</connectionPriorityTcpLan>
-            <connectionPriorityQuicLan>20</connectionPriorityQuicLan>
-            <connectionPriorityTcpWan>30</connectionPriorityTcpWan>
-            <connectionPriorityQuicWan>40</connectionPriorityQuicWan>
-            <connectionPriorityRelay>50</connectionPriorityRelay>
-            <connectionPriorityUpgradeThreshold>0</connectionPriorityUpgradeThreshold>
+            <listenAddress>$LISTEN_ADDRESS</listenAddress>
+            <globalAnnounceServer>$GLOBAL_ANNOUNCE_SERVER</globalAnnounceServer>
+            <globalAnnounceEnabled>$GLOBAL_ANNOUNCE_ENABLED</globalAnnounceEnabled>
+            <localAnnounceEnabled>$LOCAL_ANNOUNCE_ENABLED</localAnnounceEnabled>
+            <localAnnouncePort>$LOCAL_ANNOUNCE_PORT</localAnnouncePort>
+            <localAnnounceMCAddr>$LOCAL_ANNOUNCE_MC_ADDR</localAnnounceMCAddr>
+            <maxSendKbps>$MAX_SEND_KBPS</maxSendKbps>
+            <maxRecvKbps>$MAX_RECV_KBPS</maxRecvKbps>
+            <reconnectionIntervalS>$RECONNECTION_INTERVAL_S</reconnectionIntervalS>
+            <relaysEnabled>$RELAYS_ENABLED</relaysEnabled>
+            <relayReconnectIntervalM>$RELAY_RECONNECT_INTERVAL_M</relayReconnectIntervalM>
+            <startBrowser>$START_BROWSER</startBrowser>
+            <natEnabled>$NAT_ENABLED</natEnabled>
+            <natLeaseMinutes>$NAT_LEASE_MINUTES</natLeaseMinutes>
+            <natRenewalMinutes>$NAT_RENEWAL_MINUTES</natRenewalMinutes>
+            <natTimeoutSeconds>$NAT_TIMEOUT_SECONDS</natTimeoutSeconds>
+            <urAccepted>$UR_ACCEPTED</urAccepted>
+            <urSeen>$UR_SEEN</urSeen>
+            <urUniqueID>$UR_UNIQUE_ID</urUniqueID>
+            <urURL>$UR_URL</urURL>
+            <urPostInsecurely>$UR_POST_INSECURELY</urPostInsecurely>
+            <urInitialDelayS>$UR_INITIAL_DELAY_S</urInitialDelayS>
+            <autoUpgradeIntervalH>$AUTO_UPGRADE_INTERVAL_H</autoUpgradeIntervalH>
+            <upgradeToPreReleases>$UPGRADE_TO_PRE_RELEASES</upgradeToPreReleases>
+            <keepTemporariesH>$KEEP_TEMPORARIES_H</keepTemporariesH>
+            <cacheIgnoredFiles>$CACHE_IGNORED_FILES</cacheIgnoredFiles>
+            <progressUpdateIntervalS>$PROGRESS_UPDATE_INTERVAL_S</progressUpdateIntervalS>
+            <limitBandwidthInLan>$LIMIT_BANDWIDTH_IN_LAN</limitBandwidthInLan>
+            <minHomeDiskFree unit="$MIN_HOME_DISK_FREE_UNIT">$MIN_HOME_DISK_FREE_VALUE</minHomeDiskFree>
+            <releasesURL>$RELEASES_URL</releasesURL>
+            <overwriteRemoteDeviceNamesOnConnect>$OVERWRITE_REMOTE_DEVICE_NAMES_ON_CONNECT</overwriteRemoteDeviceNamesOnConnect>
+            <tempIndexMinBlocks>$TEMP_INDEX_MIN_BLOCKS</tempIndexMinBlocks>
+            <trafficClass>$TRAFFIC_CLASS</trafficClass>
+            <setLowPriority>$SET_LOW_PRIORITY</setLowPriority>
+            <maxFolderConcurrency>$MAX_FOLDER_CONCURRENCY</maxFolderConcurrency>
+            <crashReportingURL>$CRASH_REPORTING_URL</crashReportingURL>
+            <crashReportingEnabled>$CRASH_REPORTING_ENABLED</crashReportingEnabled>
+            <stunKeepaliveStartS>$STUN_KEEPALIVE_START_S</stunKeepaliveStartS>
+            <stunKeepaliveMinS>$STUN_KEEPALIVE_MIN_S</stunKeepaliveMinS>
+            <stunServer>$STUN_SERVER</stunServer>
+            <databaseTuning>$DATABASE_TUNING</databaseTuning>
+            <maxConcurrentIncomingRequestKiB>$MAX_CONCURRENT_INCOMING_REQUEST_KIB</maxConcurrentIncomingRequestKiB>
+            <announceLANAddresses>$ANNOUNCE_LAN_ADDRESSES</announceLANAddresses>
+            <sendFullIndexOnUpgrade>$SEND_FULL_INDEX_ON_UPGRADE</sendFullIndexOnUpgrade>
+            <connectionLimitEnough>$CONNECTION_LIMIT_ENOUGH</connectionLimitEnough>
+            <connectionLimitMax>$CONNECTION_LIMIT_MAX</connectionLimitMax>
+            <insecureAllowOldTLSVersions>$INSECURE_ALLOW_OLD_TLS_VERSIONS</insecureAllowOldTLSVersions>
+            <connectionPriorityTcpLan>$CONNECTION_PRIORITY_TCP_LAN</connectionPriorityTcpLan>
+            <connectionPriorityQuicLan>$CONNECTION_PRIORITY_QUIC_LAN</connectionPriorityQuicLan>
+            <connectionPriorityTcpWan>$CONNECTION_PRIORITY_TCP_WAN</connectionPriorityTcpWan>
+            <connectionPriorityQuicWan>$CONNECTION_PRIORITY_QUIC_WAN</connectionPriorityQuicWan>
+            <connectionPriorityRelay>$CONNECTION_PRIORITY_RELAY</connectionPriorityRelay>
+            <connectionPriorityUpgradeThreshold>$CONNECTION_PRIORITY_UPGRADE_THRESHOLD</connectionPriorityUpgradeThreshold>
         </options>
         <remoteIgnoredDevice></remoteIgnoredDevice>
         <pendingDevice></pendingDevice>
