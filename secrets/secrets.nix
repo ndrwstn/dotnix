10855b091998e3f96615be0d76532265258a451d
Andrew Austin <austin@impetuo.us>
2025-09-02 08:58:59 -0400

    feat(secrets): add Plutonium machine to active infrastructure
    - Add Plutonium SSH public key to secrets.nix
    - Include Plutonium in activeMachines and setupKeyMachines lists
    - Create syncthing-plutonium.age encrypted configuration file
    - Update shared syncthing.age to include Plutonium access
    
    This enables the Plutonium machine to access shared secrets and
    participate in the syncthing synchronization network.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -8,17 +8,17 @@ let
   silver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEasqUb7EN/yKS02tfVNvz8nYzgOhw0DDLz/rTR86Nw";
   # Note: Plutonium and Siberia keys will be added when those machines are activated
   # For now, using placeholder keys that will be updated when machines are deployed
-  # plutonium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPlaceholder-key-for-plutonium-will-be-updated";
+  plutonium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAYrqn/+SZCfR4TCjIUWCmn1/Os4scpP1fI/pcRtSwnn";
   # siberia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPlaceholder-key-for-siberia-will-be-updated";
 
   # Key groups for active machines
   allUsers = [ austin ];
   # activeMachines = [ monaco silver plutonium siberia ];
-  activeMachines = [ monaco silver ];
+  activeMachines = [ monaco silver plutonium ];
 
   # Machines with setup-key capability (can receive the universal setup private key)
   # setupKeyMachines = [ monaco silver plutonium siberia ];
-  setupKeyMachines = [ monaco silver ];
+  setupKeyMachines = [ monaco silver plutonium ];
 in
 {
   # Consolidated secrets with same access control as before
@@ -31,6 +31,6 @@ in
   "syncthing.age".publicKeys = allUsers ++ activeMachines; # Shared config for all machines
   "syncthing-monaco.age".publicKeys = allUsers ++ [ monaco ];
   "syncthing-silver.age".publicKeys = allUsers ++ [ silver ];
-  # "syncthing-plutonium.age".publicKeys = allUsers ++ [ plutonium ];
+  "syncthing-plutonium.age".publicKeys = allUsers ++ [ plutonium ];
   # "syncthing-siberia.age".publicKeys = allUsers ++ [ siberia ];
 }
