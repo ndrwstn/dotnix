8364e3a75b4ec48078b21e1a49cad5a2bf7de5f2
Andrew Austin <austin@impetuo.us>
2025-09-20 18:17:57 -0400

    feat: add community binary caches for faster builds
    Add nix-community.cachix.org cache for community packages and nixvim.cachix.org
    cache specifically for NixVim components (treesitter parsers, LSP servers,
    formatters). This optimization significantly reduces build times on older
    machines by downloading pre-built binaries instead of compiling from source.

--- flake.nix
+++ flake.nix
@@ -66,9 +66,13 @@
             experimental-features = [ "nix-command" "flakes" ];
             substituters = [
               "https://cache.nixos.org"
+              "https://nix-community.cachix.org"
+              "https://nixvim.cachix.org"
             ];
             trusted-public-keys = [
               "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              "nixvim.cachix.org-1:5itLbq7pKz5BB8h3QvE93s1zZOSX3XjKBpJZM+Upn7Q="
             ];
           };
         })
