bcec3286f04c94ae167fed94440df6e53d30c467
Andrew Austin <austin@impetuo.us>
2025-10-05 18:26:56 -0400

    feat(darwin): add UTF-8 locale setting to session variables
    Set LANG=en_US.UTF-8 in home.sessionVariables for Darwin machines to ensure
    proper UTF-8 locale support on macOS systems. This fixes potential character
    encoding issues in terminal applications and system tools.

--- users/austin/darwin/default.nix
+++ users/austin/darwin/default.nix
@@ -12,5 +12,7 @@
     EDITOR = "nvim";
     # 1Password SSH agent socket for macOS
     SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    # Set locale to UTF-8
+    LANG = "en_US.UTF-8";
   };
 }
