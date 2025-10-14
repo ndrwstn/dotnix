2b44f1adc1f6c80aa92763730286ecc8c48636cd
Andrew Austin <austin@impetuo.us>
2025-10-14 11:08:14 -0400

    Update broadcom-sta package versions for siberia and silver machines

--- machines/silver/configuration.nix
+++ machines/silver/configuration.nix
@@ -14,7 +14,7 @@
 
   # Allow the insecure broadcom-sta package for WiFi
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-57-6.12.51"
+    "broadcom-sta-6.30.223.271-57-6.12.52"
   ];
 
   # Use new OpenGL renderer on old MacBook Pro
