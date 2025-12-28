7d14a5409f4fd0cb320df7e0c18993e14bb2db4b
Andrew Austin <austin@impetuo.us>
2025-12-28 13:17:21 -0500

    feat(darwin): enable homebrew auto-update and upgrade on activation

--- systems/darwin/homebrew.nix
+++ systems/darwin/homebrew.nix
@@ -2,6 +2,12 @@
 { ... }: {
   homebrew = {
     enable = true;
+
+    onActivation = {
+      autoUpdate = true; # Run `brew update` before activation
+      upgrade = true; # Run `brew upgrade` for outdated packages
+      cleanup = "uninstall"; # Remove unlisted packages (preserves app data)
+    };
   };
 }
 
