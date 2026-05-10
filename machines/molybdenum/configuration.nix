2f3a2aa4ed6d268d03f6658ca7af6e54105ee3f6
Andrew Austin <austin@impetuo.us>
2026-05-10 17:42:19 -0400

    add(molybdenum): add new NixOS machine

---
+++ machines/molybdenum/configuration.nix
@@ -0,0 +1,28 @@
+# machines/molybdenum/configuration.nix
+{ config
+, pkgs
+, ...
+}: {
+  # Import secrets configuration
+  imports = [
+    ./secrets.nix
+    ./hardware-configuration.nix
+  ];
+  # Machine metadata (used by flake.nix, does not affect system configuration)
+  _astn.machineSystem = "x86_64-linux";
+  # Increase download-buffer to 1GB
+  nix.settings.download-buffer-size = 1000000000;
+
+  # Define hostname
+  networking.hostName = "Molybdenum";
+
+  # Use the systemd-boot EFI boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  system.stateVersion = "25.05";
+}
---
+++ machines/molybdenum/hardware-configuration.nix
@@ -0,0 +1,37 @@
+# machines/molybdenum/hardware-configuration.nix
+# Baseline Intel MacBook Pro NixOS hardware configuration.
+# Replace filesystem labels with generated UUIDs after installation if needed.
+{ config, lib, modulesPath, ... }:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "usbhid"
+    "usb_storage"
+    "sd_mod"
+    "sdhci_pci"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
