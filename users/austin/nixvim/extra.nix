a78cc25a356cfd05c606626276c826f4917d774c
Andrew Austin <austin@impetuo.us>
2026-06-26 14:57:57 -0400

    fix(nixvim): migrate luasnip from extraPlugins to nixvim module to fix luajit conflict

--- users/austin/nixvim/extra.nix
+++ users/austin/nixvim/extra.nix
@@ -11,8 +11,6 @@
     nvim-notify # Keep for snacks.notifier integration
     wrapping-nvim
     undotree
-    # train-nvim removed - replaced by hardtime.nvim
-    luasnip # Snippet engine for blink.cmp
   ];
 
   extraConfigLua = ''
