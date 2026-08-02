bc3e5de79c75b2871d28b761732839484b20d4de
Andrew Austin <austin@impetuo.us>
2026-08-02 11:59:16 -0400

    fix(remote-builder): pin host key and isolate SSH identity

--- features/remote-builder/default.nix
+++ features/remote-builder/default.nix
@@ -20,6 +20,11 @@ in
   config = {
     nix.distributedBuilds = cfg.sshKey != null;
     nix.settings.builders-use-substitutes = true;
+    environment.etc."ssh/ssh_config.d/90-nix-remote-builder.conf".text = ''
+      Host naphthalene.impetuo.us
+        IdentitiesOnly yes
+        IdentityAgent none
+    '';
     nix.buildMachines = lib.optional (cfg.sshKey != null) ({
       system = "x86_64-linux";
       sshUser = "austin";
@@ -29,7 +34,8 @@ in
       supportedFeatures = [ ];
       mandatoryFeatures = [ ];
       hostName = "naphthalene.impetuo.us";
-      publicHostKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw9FcJ7NSULj4LBgNAOKY1mJz/EontZB3oFPWDBI4ea8ZPR+9t6bC8+z1w+cximOKONDmWtP2ou0BDS6n8FO0YkZbp90n9VpWavvzlgCdY72Hxv/JyJaVDokeDxFXnqSwro8SH8LfovSEI+hVLq+83WaTV609H5HgJqaqoC3Q3Di242bTRwGOZqT2VvcGIwiTVsOl88y89RnGccS5jQc7Ai2/8RenY5MXW0RL6jQCsCOZTFUdlVCYPmUeD7y1feasKOr9D1AwVuqu9Pd9Vzt397uMuOiwH5oSVsuXsvhTJgtiC7TX8tsmJvMOCaioT6aKyBjTjhSYmCQpOlxUd6gbl3MENGAknb12rcG3O20KRVFu44O+vYr2UU/A1S+9r38pmHVbPgevmCw7PPgoXSErAWXgUEr3ySxlAYzb6SvshCYjMh1N6DPCsJgKgsBqTGVs+mRk1iwp7NgW6/9seC3/lU8rbtUMul/1wJscvj43CRb7/erMe/PtPXTtVlj3ctxmDNIhGCUyRLxCMA4z2znVd/7Nnmx5dNGcc3IhDQnGb4edoik7IF9rfLI36uD76HTNDWJZkE7dvK4vZeRbtwDeo8fIQUmn9D3IwAlcAFZWi/uFTfBIXV9LSwFSM/7HWnoItudvxHogD7uOKwtRdAKEjFnHu6kW6hFtOlA9lNF4pdQ==";
+      # Nix expects base64 of the complete OpenSSH public-key file.
+      publicHostKey = "c3NoLXJzYSBBQUFBQjNOemFDMXljMkVBQUFBREFRQUJBQUFDQVFDdzlGY0o3TlNVTGo0TEJnTkFPS1kxbUp6L0VvbnRaQjNvRlBXREJJNGVhOFpQUis5dDZiQzgrejF3K2N4aW1PS09ORG1XdFAyb3UwQkRTNm44Rk8wWWtaYnA5MG45VnBXYXZ2emxnQ2RZNzJIeHYvSnlKYVZEb2tlRHhGWG5xU3dybzhTSDhMZm92U0VJK2hWTHErODNXYVRWNjA5SDVIZ0pxYXFvQzNRM0RpMjQyYlRSd0dPWnFUMlZ2Y0dJd2lUVnNPbDg4eTg5Um5HY2NTNWpRYzdBaTIvOFJlblk1TVhXMFJMNmpRQ3NDT1pURlVkbFZDWVBtVWVEN3kxZmVhc0tPcjlEMUF3VnVxdTlQZDlWenQzOTd1TXVPaXdINW9TVnN1WHN2aFRKZ3RpQzdUWDh0c21Kdk1PQ2Fpb1Q2YUt5QmpUamhTWW1DUXBPbHhVZDZnYmwzTUVOR0FrbmIxMnJjRzNPMjBLUlZGdTQ0Tyt2WXIyVVUvQTFTKzlyMzhwbUhWYlBnZXZtQ3c3UFBnb1hTRXJBV1hnVUVyM3lTeGxBWXpiNlN2c2hDWWpNaDFONkRQQ3NKZ0tnc0JxVEdWcyttUmsxaXdwN05nVzYvOXNlQzMvbFU4cmJ0VU11bC8xd0pzY3ZqNDNDUmI3L2VyTWUvUHRQWFR0VmxqM2N0eG1ETkloR0NVeVJMeENNQTR6MnpuVmQvN05ubXg1ZE5HY2MzSWhEUW5HYjRlZG9pazdJRjlyZkxJMzZ1RDc2SFRORFdKWmtFN2R2SzR2WmVSYnR3RGVvOGZJUVVtbjlEM0l3QWxjQUZaV2kvdUZUZkJJWFY5TFN3RlNNLzdIV25vSXR1ZHZ4SG9nRDd1T0t3dFJkQUtFakZuSHU2a1c2aEZ0T2xBOWxORjRwZFE9PQo=";
       sshKey = cfg.sshKey;
     });
   };
