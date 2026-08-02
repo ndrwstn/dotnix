cd28acf020cd842a463bc1b216a470a0778c7c79
Andrew Austin <austin@impetuo.us>
2026-08-02 11:28:16 -0400

    fix(remote-builder): use Darwin root group for builder secret

--- features/remote-builder/secrets.nix
+++ features/remote-builder/secrets.nix
@@ -1,11 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
   age.secrets.nixbuilder-client = {
     file = ../../secrets/ssh/key-nixbuilder.age;
     path = "/run/agenix/nixbuilder-client";
     mode = "0400";
     owner = "root";
-    group = "root";
+    group = if pkgs.stdenv.isDarwin then "wheel" else "root";
   };
 
   features.remote-builder.sshKey = "/run/agenix/nixbuilder-client";
